db7651d4ea39c904bbb6caf8ab2425adba68c32d
Mike Schwab <mike.schwab@gmail.com>
2024-06-26 23:28:31 -0400

    Initial Commit

---
+++ Spindrift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spindrift
+//
+//  Created by Michael Schwab on 6/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spindrift/SpindriftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpindriftApp.swift
+//  Spindrift
+//
+//  Created by Michael Schwab on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SpindriftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
