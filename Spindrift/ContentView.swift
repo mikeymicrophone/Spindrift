1471a4eac49dea7e28136fa5f05043d19d84601e
Mike Schwab <mike.schwab@gmail.com>
2024-09-13 15:46:07 -0400

    some listreader implementation

--- Spindrift/ContentView.swift
+++ Spindrift/ContentView.swift
@@ -9,14 +9,17 @@ import SwiftUI
 import CoreStore
 import Combine
 
-
 struct ContentView: View {
     @Environment(\.dataStack) var dataStack
+    var selectas: ListPublisher<MICSelecta>
+    
     var body: some View {
+        List {
+            ListReader(self.selectas) { listSnapshot in
+                ForEach(objectIn: listSnapshot) { selecta in
+                    Text(String(describing: selecta.firstName))
+                }
+            }
         }
     }
-}
-
-#Preview {
-    ContentView()
 }
